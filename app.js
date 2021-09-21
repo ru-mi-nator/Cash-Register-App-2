7d0724aca08343a589d4f7f71c4b2afb25de192e
Ashutosh Mishra <rudrarmishra999@gmail.com>
2021-09-21 23:42:54 +0530

    wip: debug app.js

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const billAmount = document.querySelector("#bill-amount");
 const checkButton = document.querySelector("#cash-button");
 const cashGiven = document.querySelector("#cash-given");
 const errorMessage = document.querySelector("#error-message");
-
+const denomination = [2000,500,100,20,10,5,1];
+const noOfNotes = document.querySelector(".noOfNotes")
 
 checkButton.addEventListener("click", function validateCashAndBillAmount(){
-    errormessage.style.display = "none";
+    hideMessage();
     if (billAmount.value > 0) {
         if (cashGiven >= billAmount) {
             const amountToBeReturned = cashGiven - billAmount;
@@ -18,10 +19,20 @@ checkButton.addEventListener("click", function validateCashAndBillAmount(){
     }
 })
 
+function hideMessage() {
+    errormessage.style.display = "none";
+}
+
 function showMessage(message) {
     errorMessage.style.display = "block";
     errorMessage.innerText = error-message;
 }
 
-
+function calculateChange(amountToBeReturned){
+    for (let i = 0; i < denomination.length; i++) {
+        const numberOfNotes = Math.trunc(amountToBeReturned / calculateChange[i]);
+        amountToBeReturned = amountToBeReturned % calculateChange[i];
+        noOfNotes[i].innerText = numberOfNotes;
+    }
+}
 
