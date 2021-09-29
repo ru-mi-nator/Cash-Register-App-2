1766ce9fdb8696f955520d87b2a37f0e448b3f0c
Ashutosh Mishra <rudrarmishra999@gmail.com>
2021-09-29 23:59:08 +0530

    wip: changes

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ const checkButton = document.querySelector("#check-button");
 const cashGiven = document.querySelector("#cash-given");
 const errorMessage = document.querySelector("#error-message");
 const denomination = [2000, 500, 100, 20, 10, 5, 1];
-const noOfNotes = document.querySelector(".no-of-notes")
+const noOfNotes = document.querySelectorAll(".no-of-notes")
 
 checkButton.addEventListener("click", function validateBillAndCashAmount() {
     hideMessage();
     if (billAmount.value > 0) {
         if (cashGiven.value >= billAmount.value) {
             var amountToBeReturned = cashGiven.value - billAmount.value;
-            calculateChange(amountToBeReturned);
+            calculateChange(amountToBeReturned)
         } else {
             showMessage("The cash given should atleast be equal to the bill amount!!");
         }
@@ -21,7 +21,7 @@ checkButton.addEventListener("click", function validateBillAndCashAmount() {
 
 function calculateChange(amountToBeReturned) {
 
-    for (let i = 0; i < denomination.length; i++) {
+    for (let i = 0; i<denomination.length; i++) {
         const numberOfNotes = Math.trunc(amountToBeReturned / denomination[i]);
         amountToBeReturned = amountToBeReturned % denomination[i];
         noOfNotes[i].innerText = numberOfNotes;
